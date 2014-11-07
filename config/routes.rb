928d3e0ea95ce7a59e6c36d0a32b36d6f8272f96
rsilverblatt88 <rsilverblatt88@gmail.com>
2014-11-07 15:37:51 -0500

    updated styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/login" => "sessions#new"
 
   get "/hifive/unique" => "hifives#unique"
+
   resource  :session, only: [:create, :destroy]
 
   resources :users
