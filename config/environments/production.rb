fd497d8c2680c87395c6953da3e078ad44a5d9f7
rsilverblatt88 <rsilverblatt88@gmail.com>
2014-11-11 15:45:40 -0500

    added compiler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.precompile += %w(custom.css hover.css users.css works.css styleie9.css)
 end
