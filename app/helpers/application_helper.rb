edf2058f8625a7b2674678b21b1caf395e966546
rsilverblatt88 <rsilverblatt88@gmail.com>
2014-12-19 14:51:48 -0500

    removed binding.pry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@
   def hi5(phone, name, message)
     account_sid = ENV["TWILIO_API_ID"]
     auth_token = ENV["TWILIO_API_KEY"]
-    binding.pry
     client = Twilio::REST::Client.new account_sid, auth_token
 
     from = ENV["TWILIO_PHONE_NUMBER"] # Your Twilio number
