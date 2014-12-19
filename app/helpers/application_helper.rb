a50c5ead350e55ed06b679ef5d5a77659dc3c754
rsilverblatt88 <rsilverblatt88@gmail.com>
2014-12-18 20:56:08 -0500

    added api keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
  module ApplicationHelper
   def hi5(phone, name, message)
-    account_sid = "ACb4e1f1194a5932c251e20aed391154f0"
+    account_sid = ENV["TWILIO_API_ID"]
     auth_token = ENV["TWILIO_API_KEY"]
+    binding.pry
     client = Twilio::REST::Client.new account_sid, auth_token
 
     from = ENV["TWILIO_PHONE_NUMBER"] # Your Twilio number
