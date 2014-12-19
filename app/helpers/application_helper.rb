89af597e69fe6345ade578d2ba683a63653636bc
rsilverblatt88 <rsilverblatt88@gmail.com>
2014-12-18 19:40:08 -0500

    changed twilio keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
  module ApplicationHelper
   def hi5(phone, name, message)
     account_sid = "ACb4e1f1194a5932c251e20aed391154f0"
-    auth_token = "f47f7f675281fcaebf3b9f8f1788b9ac"
+    auth_token = ENV["TWILIO_API_KEY"]
     client = Twilio::REST::Client.new account_sid, auth_token
 
-    from = "+16464959475" # Your Twilio number
+    from = ENV["TWILIO_PHONE_NUMBER"] # Your Twilio number
 
     friends = {
     phone => "#{name}",
